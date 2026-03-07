99c24b40d37d4dbda7d11b27d455fbcdd03d38bd
Nitheesh20072002 <nitheeshkumar.chapala@gmail.com>
2026-03-08 02:52:46 +0530

    fixes db init

--- backend/internal/config/database.go
+++ backend/internal/config/database.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"backend/internal/models"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,12 +15,13 @@ var DB *gorm.DB
 
 func InitDB() *gorm.DB {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
